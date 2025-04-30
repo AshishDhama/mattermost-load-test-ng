8dd85d484007d7c6073cde3763e76bb5eab989c6
Alejandro García Montoro <alejandro.garciamontoro@gmail.com>
2025-04-30 17:28:31 +0200

    Show Grafana admin password in displayInfo

--- deployment/terraform/info.go
+++ deployment/terraform/info.go
@@ -16,12 +16,12 @@ func (t *Terraform) Info() error {
 		return err
 	}
 
-	displayInfo(output)
+	displayInfo(*t.GeneratedValues(), output)
 
 	return nil
 }
 
-func displayInfo(output *Output) {
+func displayInfo(genValues GeneratedValues, output *Output) {
 	fmt.Println("==================================================")
 	fmt.Println("Deployment information:")
 
@@ -66,6 +66,7 @@ func displayInfo(output *Output) {
 
 	if output.HasMetrics() {
 		fmt.Println("Grafana URL: http://" + output.MetricsServer.GetConnectionIP() + ":3000")
+		fmt.Println("    Credentials: admin / " + genValues.GrafanaAdminPassword)
 		fmt.Println("Prometheus URL: http://" + output.MetricsServer.GetConnectionIP() + ":9090")
 		fmt.Println("Pyroscope URL: http://" + output.MetricsServer.GetConnectionIP() + ":4040")
 	}
